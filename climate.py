fa49fcd6212cd167a982cecf80f868c6a7f3f34b
Abinash <abinashpatti@gmail.com>
2025-10-12 13:25:55 -0400

    Average by city and by month

--- climate.py
+++ climate.py
@@ -11,3 +11,11 @@ arr_temps = np.random.uniform(-5, 35, size=(12,3))
 print("Shape: ", arr_temps.shape)
 print("Dtype: ", arr_temps.dtype)
 print("Overall mean: ", arr_temps.mean())
+
+# Average temp per city - axis=0 for mean along rows
+arr_temps_city_avg = np.array(arr_temps.mean(axis=0))
+print("\nAverage temperature by city: ", np.round(arr_temps_city_avg, 1))
+
+# Average temp per month - axis=1 for mean along columns
+arr_temps_month_avg = np.array(arr_temps.mean(axis=1))
+print("\nAverage temperature by month: ", np.round(arr_temps_month_avg, 1))
