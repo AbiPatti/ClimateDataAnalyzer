b6d4b084c6bc19f596bfbe809e53b56855664771
Abinash <abinashpatti@gmail.com>
2025-10-19 18:50:13 -0400

    Did calculations

--- climate.py
+++ climate.py
@@ -17,23 +17,55 @@ dict_temps = {
         "Jan": 5, "Feb": 5, "Mar": 7, "Apr": 10, "May": 14, "Jun": 17,
         "Jul": 19, "Aug": 19, "Sep": 16, "Oct": 12, "Nov": 8, "Dec": 6
     },
-    
+
     "Tokyo": {
         "Jan": 5, "Feb": 6, "Mar": 9, "Apr": 15, "May": 20, "Jun": 23,
         "Jul": 26, "Aug": 27, "Sep": 24, "Oct": 18, "Nov": 12, "Dec": 8
     }
 }
 
-# Display the shape, dtype, overall mean
+# Convert data into a DataFrame
+df_temps = pd.DataFrame(dict_temps)
+
+# Display the DataFrame, shape, dtype, overall mean
+print(df_temps)
+print(df_temps.shape)
+print(df_temps.dtypes)
+print("\nOverall mean: ", df_temps.stack().mean().round(2))
 
 # Average temp per city - axis=0 for mean along rows
+print("\nAverage temperature per city: ")
+print(df_temps.mean(0))
 
 # Average temp per month - axis=1 for mean along columns
+print("\nAverage temperature per month: ")
+print(df_temps.mean(1))
 
 # Hottest and coldest temperature for each city - maximum and minimum along each column (axis=0)
+print("\nHottest temperature for each city: ")
+print(df_temps.max(0))
+print("\nColdest temperature for each city: ")
+print(df_temps.min(0))
 
 # Temperature range for each city - subtract min temp from max temp (elementwise operations)
+print("\nTemperature range for each city: ")
+print(df_temps.max(0) - df_temps.min(0))
 
-# Single hottest and single coldest temperature throughout all cities and their indexes
+# Single hottest and single coldest temperature throughout all cities
+print("\nSingle hottest temperature along all cities: ")
+print(df_temps.stack().max(0))
+print("\nSingle coldest temperature along all cities: ")
+print(df_temps.stack().min(0))
 
 # Hottest and coldest month overall - using average monthly temp array
+print("\nHottest month overall: ")
+monthly_max = df_temps.max(1)
+hottest_temp = monthly_max.max()
+hottest_month = df_temps.loc[monthly_max == hottest_temp]
+print(hottest_month)
+
+print("\nColdest month overall: ")
+monthly_min = df_temps.min(1)
+coldest_temp = monthly_min.min()
+coldest_month = df_temps.loc[monthly_min == coldest_temp]
+print(coldest_month)
